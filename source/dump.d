5eabd3100376fb15cf733a97b11a1596b6f22bb6
NotMichaelChen <mzc12345@gmail.com>
2018-11-13 00:56:38 -0500

    Implement dump_trk

--- source/dump.d
+++ source/dump.d
@@ -8,6 +8,9 @@ void mid2smf(File fp, smf_t* smf)
     writeHeader(fp, smf);
 
     dump_tempo_trk(fp, &smf.ttempo);
+
+    for(size_t i = 0; i < smf.ntracks; ++i)
+        dump_trk(fp, &(smf.tracks[i]));
 }
 
 private void writeHeader(File fp, smf_t* smf)
@@ -49,6 +52,38 @@ private void dump_tempo_trk(File fp, track_t* trk)
         //First byte is now 0, so skip it
         fp.rawWrite!ubyte(std.bitmanip.nativeToBigEndian(trk.events[i].data)[].reverse()[1..$]);
 
+    }
+
+    fp.rawWrite!ubyte([0x00, 0xFF, 0x2F, 0x00]);
+
+    ulong lenend = fp.tell();
+    fp.seek(lenloc);
+    fp.rawWrite!ubyte(std.bitmanip.nativeToBigEndian(cast(uint) (lenend - lenbegin)));
+    fp.seek(lenend);
+}
+
+void dump_trk(File fp, track_t* trk)
+{
+    fp.write("MTrk");
+    
+    //Get location of length to return & write later
+    ulong lenloc = fp.tell();
+    //Put placeholder length here
+    fp.rawWrite!ubyte([0, 0, 0, 0]);
+
+    ulong lenbegin = fp.tell();
+
+    size_t prevtime = 0;
+    for(size_t i; i < trk.nevents; i++)
+    {
+        size_t delta = trk.events[i].tick - prevtime;
+        writeVarbyte(fp, delta);
+        prevtime = trk.events[i].tick;
+
+        if(trk.events[i].bytes[0] >= 0xC0 && trk.events[i].bytes[0] <= 0xCF)
+            fp.rawWrite!ubyte(trk.events[i].bytes[1..2]);
+        else
+            fp.rawWrite!ubyte(trk.events[i].bytes[1..3]);
     }
 
     fp.rawWrite!ubyte([0x00, 0xFF, 0x2F, 0x00]);
